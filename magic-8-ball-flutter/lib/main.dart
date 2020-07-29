c15feba22aa92982a621c11f5ad25204049216a0
Tushar Rohilla <tusharrohilla.iitr@gmail.com>
2020-07-29 23:56:47 +0530

    added the magic bal app

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text(
+            'Ask Me Anything',
+          ),
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballnumber = 1;
+
+  void getnum() {
+    setState(() {
+      ballnumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballnumber.png'),
+        onPressed: () {
+          getnum();
+        },
+      ),
+    );
+  }
+}
